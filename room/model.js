b50160218da9d1872aae0aee5889e965a2b5573a
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-02-19 14:46:58 +0100

    added messages to rooms

--- room/model.js
+++ room/model.js
@@ -1,12 +1,15 @@
 const db = require("../db");
 const Sequelize = require("sequelize");
 const User = require("../user/model");
+const Message = require("../message/model");
 
 const Room = db.define("room", {
   name: Sequelize.STRING
 });
 
 User.belongsTo(Room);
+Message.belongsTo(Room);
 Room.hasMany(User);
+Room.hasMany(Message);
 
 module.exports = Room;
