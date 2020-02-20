446a03de3b1f5c8a33f298252eda2223de3ae0a0
avizmarques <aviz.marques@gmail.com>
2020-02-20 15:18:06 +0100

    expanded drawingLine model with color and user id

--- gameData/drawingLine/model.js
+++ gameData/drawingLine/model.js
@@ -3,7 +3,9 @@ const Sequelize = require("sequelize");
 
 const DrawingLine = db.define("drawingLine", {
   data: Sequelize.ARRAY(Sequelize.ARRAY(Sequelize.STRING)),
-  roomId: Sequelize.INTEGER
+  roomId: Sequelize.INTEGER,
+  color: Sequelize.ARRAY(Sequelize.FLOAT),
+  userId: Sequelize.INTEGER
 });
 
 module.exports = DrawingLine;
