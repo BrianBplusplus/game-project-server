f3f3518a007bb859cef71904e98af0b971b2997a
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-02-20 18:01:43 +0100

    resetting the database

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const db = new Sequelize(dbUrl);
 
 const syncDatabase = async () => {
   try {
-    await db.sync({ force: false });
+    await db.sync({ force: prcess.env.FORCE || false });
     console.log("Database connected");
   } catch (error) {
     console.error(error);
